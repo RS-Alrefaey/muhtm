bd5da91d1ef166facb0689810c8d255531f44ebf
munira-alduraibi <alduraibimunirah@gmail.com>
2023-10-12 16:47:37 +0300

    testing stremlit endpoint and delete old dashboard files

--- backend/muhtm/settings.py
+++ backend/muhtm/settings.py
@@ -27,7 +27,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'corsheaders',
     'rest_framework.authtoken',
 
     'rest_framework',
