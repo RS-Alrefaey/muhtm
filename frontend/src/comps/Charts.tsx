9898f210e9f7d2ea660a2725a172ce366122e38b
razan <razan.alrefaey@gmail.com>
2023-10-25 01:08:33 +0300

    fix: fix the charts

--- frontend/src/comps/Charts.tsx
+++ frontend/src/comps/Charts.tsx
@@ -18,17 +18,26 @@ function Charts({ data }: ChartsProps) {
 
   return (
     <>
-      <div className="flex flex-col w-fit">
-        <div className="flex justify-center items-center">
-          <div className="w-44">
+      <div className="flex flex-col w-full h-full px-8 py-1 space-y-6">
+        <div className="flex justify-end items-center space-x-6">
+          <div className="w-56 box-b relative flex justify-center">
+          <h1 className="text-lg font-bold text-blue-950 font-cursive absolute top-2 left-32 ">رضا العملاء</h1>
+          <div className=" p-2 w-44 h-36 relative top-2">
             <DoughnutChart data={data} />
           </div>
-          <div className="flex justify-center">
-            <h1 className="text-3xl">{lastValue as number}</h1>
+
+          </div>
+
+
+          <div className="flex flex-col text-right justify-center box-b w-36 h-36 p-4">
+            <h1 className="text-lg font-bold text-blue-950 font-cursive relative bottom-6">عدد التعليقات</h1>
+            <h1 className="text-5xl font-bold text-blue-950 font-cursive">{lastValue as number}</h1>
           </div>
         </div>
 
-        <div>
+        <div className="flex w-full h-3/4 box-b p-2 relative">
+        <h1 className="text-lg font-bold text-blue-950 font-cursive absolute top-6 right-4">احصائيات اراء العملاء</h1>
+
           <BarChart data={data} />
         </div>
       </div>
--- frontend/src/comps/DoughnutChart.tsx
+++ frontend/src/comps/DoughnutChart.tsx
@@ -1,8 +1,8 @@
 import React, { useRef, useEffect } from "react";
 import { Doughnut } from "react-chartjs-2";
-import Chart from 'chart.js/auto';
-import {CategoryScale} from 'chart.js'; 
-import agent, { BarChartArrayType } from '../API/Agent';
+import Chart from "chart.js/auto";
+import { CategoryScale } from "chart.js";
+import agent, { BarChartArrayType } from "../API/Agent";
 
 Chart.register(CategoryScale);
 
@@ -11,27 +11,50 @@ type DoughnutChartProps = {
 };
 
 function DoughnutChart({ data }: DoughnutChartProps) {
-    const allKeys = Object.keys(data);
-    const relevantKeys = allKeys.slice(8, 10);  // Get the 9th and 10th keys
+  const allKeys = Object.keys(data);
+  const relevantKeys = allKeys.slice(8, 10);
 
-    const relevantValues = relevantKeys.map(key => data[key as keyof typeof data]);
-  
-    const keyAliases: { [key: string]: string } = {
-        general_positive: "إيجابي",
-        general_negative: "سلبي",
-    };
+  const relevantValues = relevantKeys.map(
+    (key) => data[key as keyof typeof data]
+  );
 
-    const chartData = {
-      labels: relevantKeys.map(key => keyAliases[key]),
-      datasets: [
-        {
-          data: relevantValues,
-          backgroundColor: [ '#001B48', '#858585'],  // example colors
-        }
-      ],
-    };
-  
-    return <Doughnut data={chartData} />;
+  const keyAliases: { [key: string]: string } = {
+    general_positive: "إيجابي",
+    general_negative: "سلبي",
+  };
+
+  const chartData = {
+    labels: relevantKeys.map((key) => keyAliases[key]),
+    datasets: [
+      {
+        data: relevantValues,
+        backgroundColor: ["#001B48", "#858585"],
+      },
+    ],
+  };
+
+  return (
+    <Doughnut
+      data={chartData}
+      options={{
+        plugins: {
+          legend: {
+            position: "left",
+            labels: {
+              padding: 10,
+            }
+          },
+          
+        },
+        layout: {
+          padding: {
+              left: -10   
+          }
+        },
+        maintainAspectRatio: false,
+      }}
+    />
+  );
 }
-  
+
 export default DoughnutChart;
